d8ac900ab66aa51f209e7722205d06987ff26fba
Tony <anton.acipenka@gmail.com>
2021-05-28 20:39:03 +0300

    Displaying posts in the admin section

--- src/app/admin/edit-posts/edit-posts.component.ts
+++ src/app/admin/edit-posts/edit-posts.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {PostsService} from "../../service/posts.service";
+import {Observable} from "rxjs";
+import {Post} from "../../models/post";
+import {Response} from "../../models/response";
+import {PageOptions} from "../../models/pageOptions";
 
 @Component({
   selector: 'app-edit-posts',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditPostsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private postsService: PostsService) { }
+  public posts$: Observable<Response<Post>>;
+  private pageNumber = 1;
 
   ngOnInit(): void {
+    this.posts$ = this.postsService.getPosts(this.pageNumber, PageOptions.pageSize);
+  }
+
+  previousBtnClick() {
+    this.posts$ = this.postsService.getPosts(--this.pageNumber, PageOptions.pageSize);
+  }
+
+  nextBtnClick() {
+    this.posts$ = this.postsService.getPosts(++this.pageNumber, PageOptions.pageSize);
   }
 
 }
