3774857b338748c65c686a97b7f411c4e8cf296f
Tony <anton.acipenka@gmail.com>
2021-06-15 14:33:44 +0300

    Store the token in local storage and calculate the lifetime.

--- src/app/service/auth-admin.service.ts
+++ src/app/service/auth-admin.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import {environment} from "../../environments/environment";
 import {HttpClient} from "@angular/common/http";
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,5 +12,19 @@ export class AuthAdminService {
 
   authFireBase(user) {
     return this.http.post(`${environment.authFireBase}`, user)
+      .pipe(
+        tap(this.setToken)
+      )
+  }
+
+  private setToken(response) {
+    if (response) {
+      const endTokenTimeRent = new Date(new Date().getTime() + +response.expiresIn * 1000);
+      // текущая дата + миллисекунды от бэка(жизнь токена)
+      localStorage.setItem('fb-token-end', endTokenTimeRent.toString());
+      localStorage.setItem('fb-token', response.idToken);
+    } else {
+      localStorage.clear();
+    }
   }
 }
